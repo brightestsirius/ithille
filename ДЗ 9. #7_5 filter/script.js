45f34673d987d851c71d40fb26199ce2924a75c6
Olga Skliarova <sportolya@gmail.com>
2020-12-08 16:49:49 +0200

    script.js:filter

--- ДЗ 9. #7_5 filter/script.js
+++ ДЗ 9. #7_5 filter/script.js
@@ -1,60 +1,43 @@
-//ф-ция заполнения радомным числом					
-					
+//ф-ция заполнения радомным числом				
+				
 function generateArray(length=10, min=0, max=100){				
-	// console.log(arguments);				
-	let arr = new Array(10);				
-	for(let i=0; i<arr.length; i++){				
-	arr[i] = Math.floor(Math.random() * (max - min)) + min;				
-	}				
-	return arr;				
-	}				
-	let arrA = generateArray(10, -10),				
-	arrB = generateArray(12);				
-					
-	console.log(arrA);				
-					
-	//calback функция- это ф-ция,к-я нах в др-й ф-ции				
-	//ф-ция с использованием forEach				
-	// forEach – метод перебора массива.				
-	//самостоятельно запускает цикл				
-	// в круглых скобках что м.forEach делает с каждым эл-том нашего массива				
-	//т е ставим ф-цию, к-я будет работать с каждым эл-м нашего массива				
-	//приходят только три эл-та				
-	//первый ccылка на эл-т - element				
-	// второй - индекс - если с этими эл-ми не работает то и не записываем				
-	//третий - это ссылка на массив - если с этими эл-ми не работает то и не записываем				
-					
-	//ф-ция с использованием forEach				
-					
-	function workWithEachElement(element,index){				
-	element *=10;//каждый эл-т *10				
-	document.write(`<p>'значение элемента массива *10 = '${element}</p>`);				
-					
-	}				
-	console.log(arrA);//вывод эл-ты массива *10				
-	arrA.forEach(workWithEachElement);//calback функция- это ф-ция,к-я нах в др-й ф-ции				
-					
-					
-	//пример использования forEach				
-	let nomber = [1,5 ,25];				
-	let number = [100, 5 ,25];				
-					
-	nomber.forEach(				
-	function(element){				
-	document.write(`<p>'значение массива nomber ' ${element}</p>`);				
-	});//вывод на страницу 1,5,25				
-					
-	number.forEach(				
-	function(element,index,number){				
-	document.write(number.join('; '));				
-	});//вывод на страницу 100; 5; 25100; 5; 25100; 5; 25				
-					
-	//ф-ция с использованием forEach				
-	function cbForEach(element,index,array ){				
-	console.log(`index ${index} для элемента = ${element}`);				
-					
-	}				
-	arrA.forEach(cbForEach);				
-	console.log('______');				
-	arrA.forEach(cbForEach);				
-	//вывод		
+	// console.log(arguments);			
+	let arr = new Array(10);			
+	for(let i=0; i<arr.length; i++){			
+		arr[i] = Math.floor(Math.random() * (max - min)) + min;		
+	}			
+	return arr;			
+}				
+let arrA = generateArray(10, -10),				
+	arrB = generateArray(12);			
+				
+// console.log(arrA);				
+				
+//ф-ция с использованием forEach				
+function cbForEach(element,index,array ){				
+	console.log(`index ${index} для элемента = ${element}`);			
+				
+}				
+arrA.forEach(cbForEach);				
+// console.log('______');				
+arrA.forEach(cbForEach);				
+				
+//filter метод возвращает элементы массива, для которых условие истинно.				
+// Отсеивание элементов по определенном критерию.				
+//приходят только три эл-та element, индекс, ссылка на массив				
+				
+				
+newarrB = arrB.filter(cbfilter);				
+function cbfilter(element,index,array){				
+	//возвращает только если ответ на вопрос true			
+	// console.log(element,index,array);			
+	//вывод если element > 10 ,то true иначе false			
+	//return element > 10 ? true : false			
+	return element>10;			
+}				
+//созадлся новый массив и сохранятся все значения после перебора , которые отвечают вопросу элемент больше 10				
+console.log(newarrB);				
+				
+				
+//filter возвращает новый массив				
+//!!!!!!!!!!!!!!!!!!!!!!!!!				
