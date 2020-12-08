18fab1a5e51b50be269c62b74ea919808f0f0390
Olga Skliarova <sportolya@gmail.com>
2020-12-08 23:09:05 +0200

    script.js:таблица

--- ДЗ 9. #7_5 chain/script.js
+++ ДЗ 9. #7_5 chain/script.js
@@ -28,5 +28,44 @@ let resultArrAarrA = arrA
 		function(element){
 		document.write(`<div>${element}</div>`);
 		}
-	);
+	)
 // console.log(resultArrAarrA);
+
+
+
+console.log( '-------------------');
+//еще один пример
+//массив из массивов
+// фильтр не страше 18 - метод filter
+//фильтр - не замужем - метод filter
+//измениние элемента массива - метод map
+//вывод  - метод
+let users = [ 
+	['oleg', 1994 , true],
+	['ira', 1971 , true],
+	['vera', 1980 , false],
+	['ivan', 2010 , true],
+];
+
+let filterUsers = users
+	.filter(function(element){
+	console.log(element);
+	//возвращем того users у которого под вторым значением >18  
+	//и третий  значение true ( вопрос -замужем или нет)
+	return element[1]>18 && element[2] === true;
+	})
+	//измениние элемента массива
+	// index 
+	.map(function(element,index){
+		if(index%2 !== 0){
+			element.push(element[0]+' hello');
+		}
+		return element;
+	})
+	.forEach(function(element){
+		console.log(element.join(' '));
+	})
+	// console.log(filterUsers);
+console.log( '-------------------');
+
+
---
+++ ДЗ 9. #7_5 tr-строка td-колонка/script.js
@@ -0,0 +1,52 @@
+//таблица
+// вывести в таблицу
+//td - строка
+//ф-ция заполнения радомным числом					
+
+let countArray = +prompt ('сколько нужно колонок?'),
+	allArrays = [],
+	arrA = generateArray(10, -10),				
+	arrB = generateArray(12);		
+function generateArray(length=10, min=0, max=100){				
+	// console.log(arguments);				
+		let arr = new Array(10);				
+		for(let i=0; i<arr.length; i++){				
+		arr[i] = Math.floor(Math.random() * (max - min)) + min;				
+		}				
+		return arr;				
+	}				
+				
+// console.log(arrA);	
+
+function generateTR(arr){
+	//преобразовываю все в td
+	let tds = arr
+				.map(function(element){
+					return `<td>${element}</td>`;
+				})
+				.join(''); 
+		//возвращаю tr
+		return `<tr>${tds}</tr>`;
+}
+
+for(let i=0; i<countArray; i++){
+	let arr = generateArray();//вызываю ф-цию создания массива
+	allArrays.push(arr); //данные в массив
+}
+
+console.log(allArrays);//вывод -массив сформированный  user
+
+//работаем с массивом, нужно преобразовать в tr td
+let finalTable = allArrays //будет находиться массив tr
+	.map(
+		function(element){
+			// console.log(element);
+			return generateTR(element);
+		})
+		//склеить строки между собой
+		.join('')
+	console.log(finalTable);
+	//все засунуть в таблицу 
+	document.write(`<table>${finalTable}</table>`);
+
+
